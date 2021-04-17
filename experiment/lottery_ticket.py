9e80beca87a90a651007b2749500e2a064c08275
Son Hai Nguyen <sirarseus@gmail.com>
2021-04-17 23:02:34 +0200

    Refactor lottyry ticket experiment and add wandb logging

--- experiment/lottery_ticket.py
+++ experiment/lottery_ticket.py
@@ -1,5 +1,7 @@
 import json
+import wandb
 
+from typing import Dict
 from math import log
 from .prune import PruningExperiment
 
@@ -49,7 +51,6 @@ class LotteryTicketExperiment(PruningExperiment):
             logging=logging
         )
 
-        # TODO check
         self.compression = compression
         self.rewinding_it = rewinding_it
         self.pruning_rate = pruning_rate
@@ -59,48 +60,72 @@ class LotteryTicketExperiment(PruningExperiment):
         self.add_params(rewinding_it=rewinding_it)
         self.add_params(compression=compression)
 
-    def init_acc(self):
-        metrics = self.pruning_metrics()
+    def init_logger(self, **config):
+        wandb.init(project='knn-pruning', entity='sonypony', config=config)
 
-        return {v: metrics.get(v) for v in ("val_acc1", "val_acc5")}
+    def log(self, **kwargs):
+        wandb.log(kwargs)
 
-    def run(self):
-        self.freeze()
-        printc(f"Running {repr(self)}", color='YELLOW')
-        self.to_device()
-        self.build_logging(self.train_metrics, self.path)
-
-
-        target_pruning_ratio_inv = 1 / self.compression
-        pruning_iterations = round(log(target_pruning_ratio_inv, 1 - self.pruning_rate) - 1)
-
-        # train model without pruning
-        printc("Current p% level: 1", color="YELLOW")
-        # init val acc
-        metrics = self.init_acc()
-        printc("Val Acc-1: {}, Val Acc-5: {}".format(metrics.get("val_acc1"), metrics.get("val_acc5")), color="YELLOW")
-
-        self.run_epochs()
-        self.save_metrics()
-
-        for i in range(pruning_iterations):
-            print("\n\n")
-            printc("Current p% level: {}".format((1 - self.pruning_rate) ** (i + 1)), color="YELLOW")
-            target_compression_level = 1 / (1 - self.pruning_rate) ** (i + 1)
-
+    def pruning_iteration_run(self, target_compression_level: float):
+        if target_compression_level != 1.:
             # prune
             pruning_masks = self.pruning_masks(self.strategy, compression=target_compression_level)
             self.model.load_state_dict(self.k_iteration_params)
             self.apply_pruning_masks(masks=pruning_masks)
 
-            # init val acc
-            metrics = self.init_acc()
+        self.freeze()
+        self.to_device()
 
-            printc("Val Acc-1: {}, Val Acc-5: {}".format(metrics.get("val_acc1"), metrics.get("val_acc5")), color="YELLOW")
+        printc("{}\n\nCurrent p% level: {}".format("-" * 50, 1 / target_compression_level), color="YELLOW")
 
-            self.freeze()
-            self.to_device()
-            self.build_logging(self.train_metrics, self.path)
+        init_metrics = self.pruning_metrics()
+        printc("Test Init-Acc-1: {}, Test Init-Acc-5: {}".format(
+            init_metrics.get("test_acc1"), init_metrics.get("test_acc5")), color="YELLOW")
 
-            self.run_epochs()
-            self.save_metrics()
+        # train
+        self.run_epochs()
+
+        metrics = self.pruning_metrics()
+        printc(json.dumps(metrics, indent=4), color='GRASS')
+
+        # log
+        self.log(
+            test_init_acc_1=init_metrics.get("test_acc1"),
+            test_init_acc_5=init_metrics.get("test_acc5"),
+            test_acc_1=metrics.get("test_acc1"),
+            test_acc_5=metrics.get("test_acc5"),
+            p_perc=1 / target_compression_level,
+            flops=metrics.get("flops_nz"),
+            theoretical_speedup=metrics.get("theoretical_speedup"),
+        )
+
+    def run(self):
+        printc(f"Running {repr(self)}", color='YELLOW')
+
+        self.init_logger(**self.summary_params())
+
+        target_pruning_ratio_inv = 1 / self.compression
+        pruning_iterations = round(log(target_pruning_ratio_inv, 1 - self.pruning_rate) - 1)
+
+        self.pruning_iteration_run(target_compression_level=1.)
+
+        for i in range(pruning_iterations):
+            target_compression_level = 1 / (1 - self.pruning_rate) ** (i + 1)
+            self.pruning_iteration_run(target_compression_level=target_compression_level)
+
+    def summary_params(self) -> Dict:
+        return {
+            "dataset": self.params["dataset"],
+            "model": self.params["model"],
+            "seed": self.params["seed"],
+            "batch_size": self.params["dl_kwargs"]["batch_size"],
+            "split_ratio": self.params["dl_kwargs"]["split_ratio"],
+            "optim": self.params["train_kwargs"]["optim"],
+            "train_iterations": self.params["train_kwargs"]["epochs"] * len(self.train_dl),
+            "lr": self.params["train_kwargs"]["lr"],
+            "pretrained": self.params["pretrained"],
+            "warmup_iterations": self.params["warmup_iterations"],
+            "rewind_iteration": self.params["rewinding_it"],
+            "pruning_rate": self.params["pruning_rate"],
+            "strategy": self.params["strategy"]
+        }
